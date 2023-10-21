2e14d77d874284481282214955fae0912378e89c
Timothy Tandt <Timothy.tandt@outlook.be>
2023-10-21 16:52:28 +0200

    Dark Theme and Light Theme Setup

--- team-connect/app/(main)/(routes)/page.tsx
+++ team-connect/app/(main)/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
       <div>This is a protected route</div>
       <div>
         <UserButton afterSignOutUrl="/" />
+        <ModeToggle />
       </div>
     </div>
   );
