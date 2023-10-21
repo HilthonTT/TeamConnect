fcab38049389d9cfc1c67ca84b94360a22692da4
Timothy Tandt <Timothy.tandt@outlook.be>
2023-10-21 17:35:39 +0200

    Prisma setup and model creation.
    Created the models via prisma

--- team-connect/app/layout.tsx
+++ team-connect/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
+import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={font.className}>
+        <body className={cn(font.className, "bg-white dark:bg-[#2b2b2b]")}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
